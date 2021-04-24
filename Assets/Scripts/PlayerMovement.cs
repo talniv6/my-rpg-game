e85829bc83875bb2aaf6b14746c167a070e1f6d0
tal neev <tal.neev@moovit.com>
2021-04-24 12:34:15 +0300

    player movement and animation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        animator.SetFloat("horizontal", h);
+        animator.SetFloat("vertical", v);
+    }
+}
